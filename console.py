5474e81af41cd21bb5dc0cb2fcc9a6ebd05961ec
LNCarrion <lestycarrion@gmail.com>
2024-02-27 20:29:30 -0400

    adding console

--- console.py
+++ console.py
@@ -1,23 +1,24 @@
-#!/usr/bin/python3
-"""console file"""
-
-
 import cmd
 
+
 class HBNBCommand(cmd.Cmd):
+
     prompt = "(hbnb)"
 
     def do_quit(self, arg):
         """exit the program"""
         return True
+
     def help_quit(self):
         print("Quit command to exit the program")
 
     def do_EOF(self, line):
         """exit the program with (ctrl+D)"""
         return True
+
     def help_EOF(self):
         print("Exit the program when EOF is reached (Ctrl+D)")
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
